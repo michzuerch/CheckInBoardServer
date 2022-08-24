b3d4725b1412071e0c222128334c2c1206aa2e76
Michael Zuercher <michzuerch@gmail.com>
2022-08-24 14:45:41 +0200

    Connectiontest in util for Postgresql

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/michzuerch/CheckInBoardServer/config"
+	"github.com/michzuerch/CheckInBoardServer/util"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -16,6 +17,7 @@ import (
 )
 
 func main() {
+	util.CheckDatabaseAccess()
 	fmt.Printf("Test String len: %d\n", config.StringLength("Hello"))
 	err := godotenv.Load(".env")
 	if err != nil {
